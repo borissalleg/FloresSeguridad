dd8e7de27d332a0c6787840a2c98e63e98f7380d
Boris Salleg <30385889+borissalleg@users.noreply.github.com>
2025-03-19 21:05:47 -0500

    Update V1Application.java CORS configuration

--- src/main/java/com/floristeria/v1/V1Application.java
+++ src/main/java/com/floristeria/v1/V1Application.java
@@ -2,6 +2,10 @@ package com.floristeria.v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class V1Application {
@@ -9,6 +13,19 @@ public class V1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(V1Application.class, args);
 		System.out.println("Aplicacion cargada corectamente...");
+	}
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
